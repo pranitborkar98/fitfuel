ba9e41da39c79c4510e88cf1cd109195b317d585
FitFuel <pranitborkar98@gmail.com>
2026-07-07 04:22:45 +0530

    fix: Correct layout.tsx syntax for Analytics and simplify middleware

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,27 +1,39 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import { Toaster } from "react-hot-toast";
+import { Providers } from "./providers";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "FitFuel - Verified Nutrition",
-  description: "Verified intake, not self-reported. Premium meal plans delivered.",
-  metadataBase: new URL('https://fitfuel.in'),
+  title: "FitFuel - Verified Meal Plans for Serious Fitness",
+  description: "India's first verified intake meal service. No self-reporting. Just results.",
+  keywords: ["meal plan", "fitness", "nutrition", "verified intake", "FitFuel"],
+  authors: [{ name: "FitFuel" }],
   openGraph: {
-    title: "FitFuel - Verified Nutrition",
-    description: "Verified intake, not self-reported. Premium meal plans delivered.",
-    url: 'https://fitfuel.in',
-    siteName: 'FitFuel',
+    title: "FitFuel - Verified Meal Plans",
+    description: "Stop guessing. Start eating verified meals.",
+    url: "https://fitfuel.in",
+    siteName: "FitFuel",
     images: [
       {
-        url: '/og-image.png', // Make sure to add this image later
+        url: "/og-image.png", // Make sure to add this image later
         width: 1200,
         height: 630,
       },
     ],
-    locale: 'en_US',
-    type: 'website',
+    locale: "en_US",
+    type: "website",
+  },
+  twitter: {
+    card: "summary_large_image",
+    title: "FitFuel",
+    description: "Verified meal plans for serious fitness goals.",
+  },
+  robots: {
+    index: true,
+    follow: true,
   },
 };
 
@@ -36,7 +48,7 @@ export default function RootLayout({
         {/* Google Analytics */}
         <script
           async
-          src={`https://www.googletagmanager.com/gtag/js?id=G-QZ32L5KQ6X`}
+          src="https://www.googletagmanager.com/gtag/js?id=G-QZ32L5KQ6X"
         />
         <script
           dangerouslySetInnerHTML={{
@@ -48,23 +60,25 @@ export default function RootLayout({
             `,
           }}
         />
+        
         {/* Meta Pixel */}
         <script
           dangerouslySetInnerHTML={{
             __html: `
-              !function(f,b,e,v,n,t,s)
-              {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
-              n.callMethod.apply(n,arguments):n.queue.push(arguments)};
-              if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
-              n.queue=[];t=b.createElement(e);t.async=!0;
-              t.src=v;s=b.getElementsByTagName(e)[0];
-              s.parentNode.insertBefore(t,s)}(window, document,'script',
-              'https://connect.facebook.net/en_US/fbevents.js');
-              fbq('init', '1065964451892729');
-              fbq('track', 'PageView');
+              !function(f,b,e,v,n,t,s){
+                if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+                n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+                if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+                n.queue=[];t=b.createElement(e);t.async=!0;
+                t.src=v;s=b.getElementsByTagName(e)[0];
+                s.parentNode.insertBefore(t,s)}(window, document,'script',
+                'https://connect.facebook.net/en_US/fbevents.js');
+                fbq('init', '1065964451892729');
+                fbq('track', 'PageView');
             `,
           }}
         />
+
         {/* Microsoft Clarity */}
         <script
           type="text/javascript"
@@ -78,13 +92,12 @@ export default function RootLayout({
             `,
           }}
         />
-            <script async src="https://www.googletagmanager.com/gtag/js?id=G-QZ32L5KQ6X"></script>
-      <script dangerouslySetInnerHTML={{__html: \window.dataLayer=window.dataLayer||[];function gtag(){dataLayer.push(arguments);}gtag('js',new Date());gtag('config','G-QZ32L5KQ6X');\}} />
-      <script dangerouslySetInnerHTML={{__html: \!function(f,b,e,v,n,t,s){if(f.fbq)return;n=f.fbq=function(){n.callMethod?n.callMethod.apply(n,arguments):n.queue.push(arguments)};if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';n.queue=[];t=b.createElement(e);t.async=!0;t.src=v;s=b.getElementsByTagName(e)[0];s.parentNode.insertBefore(t,s)}(window,document,'script','https://connect.facebook.net/en_US/fbevents.js');fbq('init','1065964451892729');fbq('track','PageView');\}} />
-      <script dangerouslySetInnerHTML={{__html: \(function(c,l,a,r,i,t,y){c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y)})(window,document,"clarity","script","xiermgpllc");\}} />
-    </head>
+      </head>
       <body className={inter.className}>
-        {children}
+        <Providers>
+          {children}
+          <Toaster position="top-right" />
+        </Providers>
       </body>
     </html>
   );
